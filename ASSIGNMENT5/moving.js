78a9782b935c11eec50c417e9c3e32e35b895cb2
hankswildyears <henrydoesemail@gmail.com>
2017-02-08 14:23:39 -0600

    assignment 5 attempt

---
+++ ASSIGNMENT5/moving.js
@@ -0,0 +1,30 @@
+
+	function displayAnswer() {
+	firstName = document.getElementById("favoriteAnswer").value;
+  
+	document.getElementById("teamName").innerHTML = firstName;
+	}
+	
+	function displaySuccess() {
+	var yes = "Wow"
+	var no = "No Biggie"
+		if
+	(document.getElementById('answer1').checked)	{
+	document.getElementById('message').innerHTML = yes;
+		}else
+	if(document.getElementById('answer2').checked)	{
+	document.getElementById('message').innerHTML = no;
+		}
+	}
+
+
+
+function likeRomance() {
+	x = document.getElementById("romance").value;
+	document.getElementById("gameTime").innerHTML = x;
+	}
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "THEY LOVE YOU!!!!!!";
+}
